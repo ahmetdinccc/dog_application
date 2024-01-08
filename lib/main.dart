cd4667e4cf277f814f4b16ae490904c5160e8744
ATD <ahmettalhadinc50@gmail.com>
2024-01-08 13:10:17 +0300

    bottom nav bar sayfasının entegre edilmesi

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:dog_application/Pages/home.dart';
-import 'package:dog_application/Pages/settings.dart';
+
 import 'package:flutter/material.dart';
 import 'package:dog_application/Bloc/dog_cubit.dart';
-
+import 'package:dog_application/Pages/bottomnavbar.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -15,46 +14,5 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: MyHomePage());
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _currentIndex = 0;
-  final List<Widget> _pages = [const Home(), const Settings()];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages[_currentIndex],
-      bottomNavigationBar: SizedBox(
-       
-          height: 91,
-          child: BottomNavigationBar(
-            
-            backgroundColor: const Color.fromARGB(255, 243, 243, 242),
-            currentIndex: _currentIndex,
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.settings), label: 'Settings')
-          ],
-          selectedItemColor: Colors.blue,
-          ),
-          
-       
-          ),
-    );
   }
 }
