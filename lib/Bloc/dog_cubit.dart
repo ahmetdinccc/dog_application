24aadc6e76fcef3c52e1e71229157bd4955241b1
ATD <ahmettalhadinc50@gmail.com>
2024-01-08 17:06:45 +0300

    cubit oluşturlması

--- lib/Bloc/dog_cubit.dart
+++ lib/Bloc/dog_cubit.dart
@@ -1,2 +1,18 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:dog_application/Bloc/dog_state.dart';
+import 'package:dog_application/Repository/dog_repository.dart';
 
-  
+class DogCubit extends Cubit<DogState> {
+  final DogRepository _dogRepository;
+  DogCubit(this._dogRepository) : super(DogInitial());
+
+  Future<void> getDog() async {
+    try {
+      emit(DogLoading());
+      final response = await _dogRepository.getDog();
+      emit(DogCompleted(response));
+    } on NetworkError catch (_) {
+      emit(DogError("Try Searching with another word"));
+    }
+  }
+}
