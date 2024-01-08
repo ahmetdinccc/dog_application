33b61044b6d7fd72e23591983a3b2372cf601b00
ATD <ahmettalhadinc50@gmail.com>
2024-01-08 13:09:47 +0300

    bottom nav bar sayfasının oluşturulması

---
+++ lib/Pages/bottomnavbar.dart
@@ -0,0 +1,44 @@
+
+import 'package:flutter/material.dart';
+import 'package:dog_application/Pages/home.dart';
+import 'package:dog_application/Pages/settings.dart';
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+  final List<Widget> _pages = [const Home(), const Settings()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: SizedBox(
+       
+          height: 91,
+          child: BottomNavigationBar(
+            
+            backgroundColor: const Color.fromARGB(255, 243, 243, 242),
+            currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), label: 'Settings')
+          ],
+          selectedItemColor: Colors.blue,
+          ),
+          
+       
+          ),
+    );
+  }
+}
