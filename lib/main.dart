420f195cad420890d8905a3f1f4b93d6abcb6977
ATD <ahmettalhadinc50@gmail.com>
2024-01-08 17:06:30 +0300

    gereksiz importların silinmesi

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-
 import 'package:flutter/material.dart';
-import 'package:dog_application/Bloc/dog_cubit.dart';
 import 'package:dog_application/Pages/bottomnavbar.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage());
+        debugShowCheckedModeBanner: false, 
+        home: MyHomePage());
   }
 }
